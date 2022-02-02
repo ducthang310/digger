b303ce0b1ac386967e66142043c83a08f691ac48
Thang <ducthang310@gmail.com>
2022-02-02 23:46:33 +0700

    Rich Text

--- src/painter/services/text.service.ts
+++ src/painter/services/text.service.ts
@@ -11,43 +11,53 @@ export class TextService extends PointService {
             draggable: data.draggable,
             rotation: data.rotation,
         });
-        const padding = 0;
-        const rect = new Konva.Rect({
-            x: 0,
-            y: 0,
-            width: 100,
-            height: 50,
-            fill: data.textBackgroundColor,
-        });
-        // const simpleText = new Konva.Text({
-        //     x: padding,
-        //     y: padding,
-        //     text: data.text,
-        //     fontSize: 14,
-        //     fill: data.textColor,
-        //     fontStyle: '400',
-        //     fontFamily: 'Poppins',
-        //     lineHeight: 1.25,
-        // });
+        let padding = 0;
+        let mainText: Konva.Shape;
+        let textScale = 1;
+        let rect: Konva.Rect;
+        if (!data.text_canvas) {
+            padding = 3;
+            const textStr = data.text.replace(/(<([^>]+)>)/gi, "");
+            rect = new Konva.Rect({
+                x: 0,
+                y: 0,
+                width: 100,
+                height: 50,
+                fill: data.textBackgroundColor,
+            });
+            mainText = new Konva.Text({
+                x: padding,
+                y: padding,
+                text: textStr,
+                fontSize: 14,
+                fill: data.textColor,
+                fontStyle: '400',
+                fontFamily: 'Poppins',
+                lineHeight: 1.25,
+            });
+        } else {
+            textScale = 1 / window.devicePixelRatio;
+            mainText = new Konva.Image({
+                x: 0,
+                y: 0,
+                scaleX: textScale,
+                scaleY: textScale,
+                image: data.text_canvas,
+            });
+        }
 
-        const textScale = 1 / window.devicePixelRatio;
-        const simpleText = new Konva.Image({
-            x: 0,
-            y: 0,
-            scaleX: textScale,
-            scaleY: textScale,
-            image: data.text_canvas,
-        });
-        // const textScale = 1;
-        const textWidth = simpleText.width() * textScale;
-        const textHeight = simpleText.height() * textScale;
+        const textWidth = mainText.width() * textScale;
+        const textHeight = mainText.height() * textScale;
 
         const rectWidth = textWidth + padding * 2;
-        rect.width(rectWidth);
         const rectHeight = textHeight + padding * 2;
-        rect.height(rectHeight);
-        point.add(rect);
-        point.add(simpleText);
+        if (rect) {
+            rect.width(rectWidth);
+            rect.height(rectHeight);
+            point.add(rect);
+        }
+
+        point.add(mainText);
         if (data.skewX) {
             point.skewX(Math.tan( Number(data.skewX) * Math.PI/180 ));
         }
@@ -64,26 +74,42 @@ export class TextService extends PointService {
 
     changePointProperties(pointData: PainterPointInterface, point: Konva.Group): void {
         point.position(pointData.position);
-        const konText: Konva.Text = point.findOne('Text');
-        if (!pointData.text && konText) {
-            konText.destroy();
-        } else if (pointData.text) {
-            if (!konText) {
-                const newKonText = new Konva.Text({
-                    text: pointData.text,
-                    fontSize: 14,
-                    fill: pointData.textColor,
-                    fontStyle: '400'
-                });
-                point.add(newKonText);
-            } else {
-                konText.fill(pointData.textColor);
-                konText.text(pointData.text);
-            }
+        if (pointData.text_canvas) {
+            const textScale = 1 / window.devicePixelRatio;
+            const mainText = new Konva.Image({
+                x: 0,
+                y: 0,
+                scaleX: textScale,
+                scaleY: textScale,
+                image: pointData.text_canvas,
+            });
+            const konText = point.findOne('Text');
+            const konRect = point.findOne('Rect');
+            konText && konText.destroy();
+            konRect && konRect.destroy();
+            point.add(mainText);
+        } else {
+            const konText: Konva.Text = point.findOne('Text');
+            if (!pointData.text && konText) {
+                konText.destroy();
+            } else if (pointData.text) {
+                if (!konText) {
+                    const newKonText = new Konva.Text({
+                        text: pointData.text,
+                        fontSize: 14,
+                        fill: pointData.textColor,
+                        fontStyle: '400'
+                    });
+                    point.add(newKonText);
+                } else {
+                    konText.fill(pointData.textColor);
+                    konText.text(pointData.text);
+                }
 
-            const konRect: Konva.Rect = point.findOne('Rect');
-            if (konRect) {
-                konRect.fill(pointData.textBackgroundColor);
+                const konRect: Konva.Rect = point.findOne('Rect');
+                if (konRect) {
+                    konRect.fill(pointData.textBackgroundColor);
+                }
             }
         }
     }
